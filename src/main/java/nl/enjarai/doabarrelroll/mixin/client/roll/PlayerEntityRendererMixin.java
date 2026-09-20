b8c9987bc6b59856bf01fcbf0e541e176ef958b0
Tim <tim.gammenthaler@gmail.com>
2026-09-20 18:08:55 +0200

    Fix mixin target: MatrixStack.multiply now takes Quaternionfc, not Quaternionf

--- src/main/java/nl/enjarai/doabarrelroll/mixin/client/roll/PlayerEntityRendererMixin.java
+++ src/main/java/nl/enjarai/doabarrelroll/mixin/client/roll/PlayerEntityRendererMixin.java
@@ -1,13 +1,11 @@
 package nl.enjarai.doabarrelroll.mixin.client.roll;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
 import net.minecraft.util.math.RotationAxis;
-import nl.enjarai.doabarrelroll.api.RollEntity;
 import nl.enjarai.doabarrelroll.api.RollRenderState;
-import org.joml.Quaternionf;
+import org.joml.Quaternionfc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -18,13 +16,12 @@ public abstract class PlayerEntityRendererMixin {
             method = "setupTransforms(Lnet/minecraft/client/render/entity/state/PlayerEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;FF)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/util/math/MatrixStack;multiply(Lorg/joml/Quaternionf;)V",
+                    target = "Lnet/minecraft/client/util/math/MatrixStack;multiply(Lorg/joml/Quaternionfc;)V",
                     ordinal = 1
             ),
             index = 0
     )
-    private Quaternionf doABarrelRoll$modifyRoll(Quaternionf original,
-                                                 @Local(argsOnly = true) PlayerEntityRenderState state) {
+    private Quaternionfc doABarrelRoll$modifyRoll(Quaternionfc original, @Local(argsOnly = true) PlayerEntityRenderState state) {
         var rollState = (RollRenderState) state;
 
         if (rollState.doABarrelRoll$isRolling()) {
