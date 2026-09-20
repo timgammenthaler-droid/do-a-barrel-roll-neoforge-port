f630598aad01adc17e12b9e8f0fa6a87a78d5f71
Tim <tim.gammenthaler@gmail.com>
2026-09-20 17:32:11 +0200

    Fix drawTexture RenderPipeline API change

--- src/main/java/nl/enjarai/doabarrelroll/util/StarFoxUtil.java
+++ src/main/java/nl/enjarai/doabarrelroll/util/StarFoxUtil.java
@@ -1,8 +1,8 @@
 package nl.enjarai.doabarrelroll.util;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -53,7 +53,7 @@ public class StarFoxUtil {
             int x = scaledWidth / 2 - 75;
             int y = scaledHeight - 90;
             int texture = barrelRollTimer % 2 == 0 ? 1 : 2;
-            context.drawTexture(RenderLayer::getGuiTextured, texture == 1 ? barrelRollTexture1 : barrelRollTexture2,
+            context.drawTexture(RenderPipelines.GUI_TEXTURED, texture == 1 ? barrelRollTexture1 : barrelRollTexture2,
                     x, y, 0, 0, 160, 160, 160, 160);
         }
     }
